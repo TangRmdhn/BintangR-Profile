af86c66c9412f3978ee9feb820c752ed602ac44b
TangRmdhn <samsa2731@gmail.com>
2025-02-06 00:55:31 +0700

    Test 3.0

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const scroller = document.querySelector('.scroller');
-const container = document.querySelector('.container-scroller');
+const container = document.querySelector('.container-scroll');
 
-// Clone konten sampai cukup panjang
-function duplicateContent() {
-
-    scroller.innerHTML += scroller.innerHTML;
-    scroller.innerHTML += scroller.innerHTML;
-
-    while(scroller.scrollWidth < container.offsetWidth * 2) {
-        scroller.innerHTML += scroller.innerHTML;
+// Pastikan elemen ada sebelum dijalankan
+if (scroller && container) {
+    function duplicateContent() {
+        let content = scroller.innerHTML;
+        while (scroller.scrollWidth < container.offsetWidth * 2) {
+            scroller.innerHTML += content;
+        }
     }
+
+    duplicateContent();
+
+    // Reset animasi untuk menghindari jeda
+    scroller.style.animation = 'none';
+    setTimeout(() => {
+        scroller.style.animation = '';
+    }, 10);
 }
 
-duplicateContent();
+// Menambahkan event listener ke tombol burger
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
 
-// Reset animasi untuk menghindari jeda
-scroller.style.animation = 'none';
-setTimeout(() => {
-    scroller.style.animation = '';
-}, 10);
-
+if (burger && menu) {
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+}
